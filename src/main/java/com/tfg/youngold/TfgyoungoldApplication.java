10b003980d85cf50e0e9c182e0e1b80093621aed
aojalvog <alvaro.ojalvo@gmail.com>
2024-05-20 12:17:58 +0200

    feat(Creación del proyecto): :sparkles:

---
+++ src/main/java/com/tfg/youngold/TfgyoungoldApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.youngold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgyoungoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgyoungoldApplication.class, args);
+	}
+
+}
